74a466c97975cd22ebb90af77b4b108723e9f926
Budali11 <m13058266662@163.com>
2022-11-05 23:15:11 +0800

    normal update

---
+++ chapter_1/build/Src/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/daytimetcpcli.c.o"
+  "CMakeFiles/main.dir/Lib/error.c.o"
+  "../../bin/main.pdb"
+  "../../bin/main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
